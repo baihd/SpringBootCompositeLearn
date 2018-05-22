70bef59aa19ec7c06f1856e926f1005931cf8cd1
baihd <847350737@qq.com>
2018-05-22 14:42:20 +0800

    add springboot-restdocs

---
+++ springboot-restdocs/src/main/java/com/composite/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.composite.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public Map<String, String> greeting() {
+        return Collections.singletonMap("message", "Hello World");
+    }
+}
