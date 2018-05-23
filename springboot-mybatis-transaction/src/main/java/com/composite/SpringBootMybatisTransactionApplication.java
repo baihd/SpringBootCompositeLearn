6b0ccff8785ca3abc5f6821fcdeb0fd1195c58ed
baihd <847350737@qq.com>
2018-05-23 14:59:09 +0800

    add scheduled

--- springboot-mybatis-transaction/src/main/java/com/composite/SpringBootMybatisTransactionApplication.java
+++ springboot-mybatis-transaction/src/main/java/com/composite/SpringBootMybatisTransactionApplication.java
@@ -3,9 +3,11 @@ package com.composite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableScheduling
 public class SpringBootMybatisTransactionApplication {
 
     public static void main(String[] args) {
